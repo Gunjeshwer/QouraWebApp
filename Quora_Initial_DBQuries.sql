1353eaee28e242180e7cd6dc9b08d1dd20ee0236
Gunjeshwer <66192595+Gunjeshwer@users.noreply.github.com>
2020-08-23 19:51:12 +0530

    Add files via upload

---
+++ Quora_Initial_DBQuries.sql
@@ -0,0 +1,63 @@
+Create database QuoraDatabase
+go
+
+use QuoraDatabase
+go
+
+Create table Categories(
+categoryID int primary key identity(1,1),
+categoryName nvarchar(max))
+go
+
+create table Users(
+UserID int primary key identity(1,1),
+Email nvarchar(max),
+PasswordHash nvarchar(max),
+Name nvarchar(max),
+Mobile nvarchar(max),
+IsAdmin bit default(0))
+go
+
+create table Questions(
+QuestionID int primary key identity(1,1),
+QuestionName nvarchar(max),
+QuestionDateAndTime datetime,
+UserID int references Users(UserID) on delete cascade,
+CategoryID int references Categories(CategoryID) on delete cascade,
+VotesCount int,
+AnswersCount int,
+ViewCount int)
+go
+
+create table Answers(
+AnswerID int primary key identity(1,1),
+AnswerText nvarchar(max),
+AnswerDateAndTime datetime,
+UserID int references Users(UserID),
+QuestionID int references Questions(QuestionID) on delete cascade,
+VoteCount int)
+go
+
+create table Votes(
+VoteID int primary key identity(1,1),
+UserID int references Users(UserID),
+AnswerID int references Answers(AnswerID) on delete cascade,
+VoteValue int)
+go
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
